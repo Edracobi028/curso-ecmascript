03c8222cf1c2f5604a0f8fa94e758e2c399edd2a
DEVASM <erazo@agristar.com.mx>
2023-09-14 18:57:53 -0600

    ES6: promesas 11/35

---
+++ src/es6/06-primises.js
@@ -0,0 +1,16 @@
+//Promises (cosas que en algún momento tendrán que pasar, hoy mañana o nunca)
+
+//Hacer una constante con arrow function que retorne una promesa
+const anotherFunction = () => {
+    return new Promise((resolve, reject) =>{
+        if(false){
+            resolve('Yeah!!');
+        }else{
+            reject('Whooops!');
+        }
+    })//retornar una Arrow function anonima pasandole 2 parametros 
+}
+
+anotherFunction()
+.then(response => console.log(response))//.then nos retornamos un response y returnaremos directo un console log de la respuesta
+.catch(err => console.log("error = " + err)); //catch es por si no logra resolverse de forma exitosa la promesa, nos retorna un error y retornamos directo para ver el error en consola 
