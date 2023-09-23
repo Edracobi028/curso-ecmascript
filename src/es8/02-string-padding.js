7d69a0fd919cffdbd41882fd24c6ad816def9f3f
DEVASM <erazo@agristar.com.mx>
2023-09-23 13:46:31 -0600

    ES8: string padding y trailing commas 20/35

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,9 @@
+// 
+
+const string = 'Hello';
+
+//Agrega caracteres al inicio como padding indicando el string y cuantas posiciones rellena
+console.log(string.padStart(6, '_'));
+
+//Agrega caracteres al final como padding indicando el string y cuantas posiciones rellena
+console.log(string.padEnd(6, '_'));
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,7 @@
+//Trailing commas : tratar de acceder a ellos con trailing commas como Js detecta estacios en blanco en arreglos
+//Crear un Array de numeros
+const array = [24,34, 25, 24, , , , 45];
+console.log(array);
+console.log(array.length);
+
+
