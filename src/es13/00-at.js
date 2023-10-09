f1e99e39f730d3938168adf42fca7268d53982a2
Eduardo Razo <esse_rios_e@hotmail.com>
2023-10-08 22:22:10 -0600

    ES13: at 33/35

---
+++ src/es13/00-at.js
@@ -0,0 +1,7 @@
+//ES13 2022
+//at: funcion abreviada que indica el total de elementos de un array
+const array = ["one", "two", "three", "four", "five", "six"];
+
+console.log(array[array.length - 1]); //six
+
+console.log(array.at(-1)); //six
