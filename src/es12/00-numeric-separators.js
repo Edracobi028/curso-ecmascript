973216b133c5e9b857f67a74a0c0dd93d5057076
Eduardo Razo <esse_rios_e@hotmail.com>
2023-10-08 21:54:34 -0600

    ES12: numeric-separators y replaceAll 31/35

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,6 @@
+//ES12 Junio del 2021
+//Numeric Separaots: Separar con guian bajo sin y el sistema lo interpreta natural (sin los simbolos)
+
+const value = 10_000_000_000;
+value = value + 1;
+console.log(value); //10000000001
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+//Replace: Transformar algunos textos, reemplazar. 
+
+const string = 'JavaScript es un maravilloso lenguaje de programación';
+//1er argumento que va a buscar, a remplazar
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+console.log(replacedString);
